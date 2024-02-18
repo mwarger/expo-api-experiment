7fe4680de1e9559f966ecd38c0938de148af8639
Mat Warger <686823+mwarger@users.noreply.github.com>
2024-02-18 12:02:31 -0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+import { Button } from 'react-native';
+
+async function fetchHello() {
+  console.log('fetchHello');
+  const response = await fetch('/api/hello');
+  const data = await response.json();
+  alert('Hello ' + data.hello);
+}
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <EditScreenInfo path='app/(tabs)/index.tsx' />
+      <Button onPress={() => fetchHello()} title='Fetch hello' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
